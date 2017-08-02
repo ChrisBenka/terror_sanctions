5bff4bd1c11994e4c3eeb45724452652596a625e
Chris Benka <chrisbenka@gwmail.gwu.edu>
2017-08-01 22:57:23 -0400

    backend authentication stable

--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -1,18 +1,11 @@
 defmodule Terror.UserView do
   use Terror.Web, :view
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, Terror.UserView, "user.json")}
-  end
-
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, Terror.UserView, "user.json")}
-  end
-
   def render("user.json", %{user: user}) do
-    %{id: user.id,
+    %{
+      id: user.id,
       username: user.username,
       email: user.email,
-      password_hash: user.password_hash}
+    }
   end
 end
