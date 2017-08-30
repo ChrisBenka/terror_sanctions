79b244f0d6e63de4e50b32fd0923f56d3574ca2a
chrisb-rep <chris@represently.com>
2017-08-29 21:13:58 -0400

    added leaflet-cluster rendering

--- react/src/components/Map/index.js
+++ react/src/components/Map/index.js
@@ -1,43 +1,43 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
+import Loader from '../Loader';
+
+const fillMarkers = (individuals) => {
+  let markers = [];
+  for(let i =0;i<individuals.length;i++){
+    markers.push({lat: individuals[i].geo_loc.coordinates[0],
+      lng:individuals[i].geo_loc.coordinates[1],
+      popup: '<span>' + individuals[i].name +'</span>'});
+  }
+  return markers;
+}
 
 class GlobalMap extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13,
-    };
   }
-//
   render() {
-    const { individuals } = this.props
+    const { individuals } = this.props;
     if(individuals.length>0){
-      console.log(individuals[0].geo_loc.coordinates[0])
-      const position = [individuals[0].geo_loc.coordinates[0],(individuals[0].geo_loc.coordinates[1] * -1)]
+     let markers = fillMarkers(individuals);
     return (
-      <Map center={position} zoom={6}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>Christopher Benka <br /> Report Link here.</span>
-          </Popup>
-        </Marker>
-      </Map>
+    <Map className="markercluster-map" center={[51.0, 19.0]} zoom={4} maxZoom={18}>
+      <TileLayer
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <MarkerClusterGroup
+        markers={markers}
+        wrapperOptions={{enableDefaultStyle: true}}
+      />
+    </Map>
     );
   }
-    return <div> loading... </div>
+    return <div> Loading... </div>;
   }
 }
 export default GlobalMap;
 
 
-/*
-  Create list of markers 
-  render all of htose markerss
-
-*/
+//create list of markers 
