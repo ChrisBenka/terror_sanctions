7fcb8bd495e1f219ccee6d6c175c2839302c97dd
chrisb-rep <chris@represently.com>
2017-08-30 16:23:57 -0400

    progress on add Individual form

--- api/web/controllers/individual_controller.ex
+++ api/web/controllers/individual_controller.ex
@@ -8,19 +8,41 @@ defmodule Terror.IndividualController do
     render(conn, "index.json", individuals: individuals)
   end
 
-  def create(conn, %{"individual" => individual_params}) do
-    changeset = Individual.changeset(%Individual{}, individual_params)
+  def create(conn,individual_params) do
+    get_geo_loc(conn,individual_params)
+    test =  %{name: individual_params["name"],location: individual_params["location"],
+    date_of_birth: individual_params["date_of_birth"], place_of_birth: individual_params[""],
+    report_title: individual_params["report_title"], report: individual_params["report"],
+    sources: individual_params["sources"]}    
+    # changeset = Individual.changeset(%Individual{}, individual_params)
 
-    case Repo.insert(changeset) do
-      {:ok, individual} ->
-        conn
-        |> put_status(:created)
-        |> put_resp_header("location", individual_path(conn, :show, individual))
-        |> render("show.json", individual: individual)
-      {:error, changeset} ->
+    # case Repo.insert(changeset) do
+    #   {:ok, individual} ->
+    #     conn
+    #     |> put_status(:created)
+    #     |> put_resp_header("location", individual_path(conn, :show, individual))
+    #     |> render("show.json", individual: individual)
+    #   {:error, changeset} ->
+    #     conn
+    #     |> put_status(:unprocessable_entity)
+    #     |> render(Terror.ChangesetView, "error.json", changeset: changeset)
+    # end
+  end
+  defp get_geo_loc(conn,individual_params) do
+    IO.inspect(individual_params)
+    HTTPoison.start
+    case HTTPoison.get("https://maps.googleapis.com/maps/api/geocode/json",[],
+    params: %{address: individual_params["location"],key: "AIzaSyCtHMUj7UBHBI53TIlzNqa4JgninhLrzbk"}) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        json = Poison.decode!(body)
+        json = Enum.at(json["results"],0)
+        json = json["geometry"]["location"]
+        geom = %Geo.Point{ coordinates: {json["lat"], json["lng"]}}
+        IO.inspect(geom)
+      {:error, changset} ->
         conn
         |> put_status(:unprocessable_entity)
-        |> render(Terror.ChangesetView, "error.json", changeset: changeset)
+        |>render(Terror.ChangesetView,"error.json",changset: changset)
     end
   end
 
